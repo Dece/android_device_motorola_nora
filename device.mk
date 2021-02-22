8dd53dd6ab6990967634d58588dc5819d31616cf
the-flash <flash@remainsilent.net>
2021-02-22 22:47:34 +0000

    nora: configs: Import camera configs from stock
    Signed-off-by: the-flash <flash@remainsilent.net>

--- device.mk
+++ device.mk
@@ -8,10 +8,10 @@
 $(call inherit-product, device/motorola/msm8937-common/msm8937.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/motorola/james/james-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/nora/nora-vendor.mk)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 720
 
 # Overlays
@@ -29,11 +29,13 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/camera/mot_hi556_byd_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_hi556_byd_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/mot_hi556_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_hi556_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/mot_hi846_byd_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_hi846_byd_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/mot_hi846_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_hi846_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/msm8917_mot_james_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8917_mot_james_camera.xml
+    $(LOCAL_PATH)/configs/camera/mot_s5k4h8_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_s5k4h8_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/msm8917_mot_jeter_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8917_mot_jeter_camera.xml \
+    $(LOCAL_PATH)/configs/camera/ov5675_jeterht_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov5675_jeterht_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/ov5675_jeterkc_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov5675_jeterkc_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/s5k3l8_jeterht_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3l8_jeterht_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/s5k3l8_jetertr_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3l8_jetertr_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/s5k4h7_jeterof_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k4h7_jeterof_chromatix.xml
 
 # Keylayout
 PRODUCT_COPY_FILES += \
@@ -43,17 +45,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-egis.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-egis.idc
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/unavail.android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unavail.android.hardware.fingerprint.xml \
-    $(LOCAL_PATH)/configs/permissions/unavail.android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unavail.android.hardware.sensor.gyroscope.xml
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
 
 PRODUCT_PACKAGES += \
-    init.mmi.laser.sh \
     init.qcom.sensors.sh
 
 # Releasetools script
