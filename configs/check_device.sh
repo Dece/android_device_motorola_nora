e974893d1672bd05e283a0ba5892721ba1973923
the-flash <flash@remainsilent.net>
2021-02-24 08:16:29 +0000

    nora: Drop vulkan from check_device.sh
    No longer needed
    
    Signed-off-by: the-flash <flash@remainsilent.net>

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -3,6 +3,3 @@
 SKU=`getprop ro.boot.hardware.sku`
 
 rm -rf /vendor/app/LineageActions
-rm /vendor/etc/permissions/android.hardware.vulkan.compute-0.xml
-rm /vendor/etc/permissions/android.hardware.vulkan.level-0.xml
-rm /vendor/etc/permissions/android.hardware.vulkan.version-1_1.xml
